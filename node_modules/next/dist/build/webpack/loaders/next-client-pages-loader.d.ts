4c5f3a15e169e0371c6f0e08682f866a9e59a7f3
Edgar López <edishlpz@gmail.com>
2020-05-02 14:15:26 -0500

    remove gitignore files

--- node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.d.ts
+++
@@ -1,8 +0,0 @@
-import { loader } from 'webpack';
-export declare type ClientPagesLoaderOptions = {
-    absolutePagePath: string;
-    page: string;
-    hotRouterUpdates: boolean;
-};
-declare const nextClientPagesLoader: loader.Loader;
-export default nextClientPagesLoader;
