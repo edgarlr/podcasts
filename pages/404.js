05b122249e52ecf1be1ef8c9c8f25f6cfce3481a
Edgar López <edgar.lr.18@gmail.com>
2020-09-01 22:31:45 -0500

    Update next and react-icosn, update package.json and 404 page created

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1>404. Page Not Found</h1>
+    </div>
+  )
+}
+
+import React from 'react'
+
--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,23 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
-      <html>
+      <Html>
         <Head />
         <body>
           <Main />
           <div id='audioPlayer' />
           <NextScript />
         </body>
-      </html>
-    );
+      </Html>
+    )
   }
 }
+
+export default MyDocument
