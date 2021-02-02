1bfc9fc239818d38e3eeeeeca486d53b824aa365
Edgar López <edgar.lr.18@gmail.com>
2021-02-02 16:27:22 -0600

    Remove old use favs hook

--- lib/contexts/index.ts
+++ lib/contexts/index.ts
@@ -2,6 +2,3 @@ import { createContext, useContext } from 'react'
 
 export const PlayerContext = createContext<TPlayerContext | null>(null)
 export const usePlayer = () => useContext(PlayerContext)
-
-export const FavsContext = createContext(null)
-export const useFavs = () => useContext(FavsContext)
