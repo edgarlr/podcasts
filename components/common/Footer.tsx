e9095e2dff28127bd6365acf3664089b12fc1ccb
Edgar López <edgar.lr.18@gmail.com>
2021-02-09 17:58:13 -0600

    Fix footer styles

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,16 +1,19 @@
 import Github from '@components/icons/Github'
-import Twitter from '@components/icons/Twitter'
 import ExternalLink from '@components/ui/ExternalLink'
-import { GITHUB_URL, TWITTER_URL } from '@lib/constants'
+import { SOURCE_URL } from '@lib/constants'
+import { usePlayer } from '@lib/hooks/use-player'
 
 const Footer = () => {
+  const { current } = usePlayer()
   return (
-    <footer>
-      <ExternalLink to={TWITTER_URL} ariaLabel="Link to twitter">
-        <Twitter />
-      </ExternalLink>
-      <ExternalLink to={GITHUB_URL} ariaLabel="Link to source code">
-        <Github />
+    <footer className={current ? 'playing' : ''}>
+      <ExternalLink
+        to={SOURCE_URL}
+        ariaLabel="Link to source code"
+        className="source-link"
+      >
+        <Github width={18} height={18} style={{ marginRight: '.5rem' }} />
+        Source
       </ExternalLink>
 
       <style jsx>{`
@@ -19,6 +22,25 @@ const Footer = () => {
           display: flex;
           justify-content: center;
           gap: 1rem;
+        }
+        footer > :global(.source-link) {
+          display: flex;
+          align-items: center;
+        }
+        @media screen and (min-width: 766px) {
+          footer.playing > :global(.back-btn) {
+            left: 1.5rem;
+          }
+        }
+        @media screen and (min-width: 1024px) {
+          footer.playing {
+            padding: 2rem 21rem 2rem 1.5rem;
+          }
+        }
+        @media screen and (min-width: 1440px) {
+          footer.playing {
+            padding: 2rem 23rem 2rem 1.5rem;
+          }
         }
       `}</style>
     </footer>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
 export const GITHUB_URL = 'https://github.com/edgarlr'
 export const TWITTER_URL = 'https://twitter.com/@edgarlr_'
+export const SOURCE_URL = 'https://github.com/edgarlr/podcasts'
 
 export const SITE_URL = 'https://podcast-app.vercel.app'
