346acd20f556b5eea1356576965c2c6459ee9a12
Edgar López <edgar.lr.18@gmail.com>
2021-04-12 02:20:32 -0500

    Add new favicons

--- components/common/head.tsx
+++ components/common/head.tsx
@@ -23,98 +23,38 @@ const Head = () => {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
-      {/* General and favicon */}
+      {/* General and Manifest */}
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-      <link rel="manifest" href="/static/manifest.json" key="site-manifest" />
-
-      <meta name="theme-color" content="#ffffff" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
       <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+
+      {/* ICONS */}
       <meta
         name="msapplication-TileImage"
         content="/static/images/ms-icon-144x144.png"
       />
-
-      {/* Icons */}
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/static/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="mask-icon"
-        href="/static/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-
-      <link
-        rel="apple-touch-icon"
-        sizes="57x57"
-        href="/static/images/apple-icon-57x57.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="60x60"
-        href="/static/images/apple-icon-60x60.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href="/static/images/apple-icon-72x72.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href="/static/images/apple-icon-76x76.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/static/images/apple-icon-114x114.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/static/images/apple-icon-120x120.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="/static/images/apple-icon-144x144.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/static/images/apple-icon-152x152.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/static/images/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/static/images/android-icon-192x192.png"
+        href="/favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/static/images/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/static/images/favicon-96x96.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/static/images/favicon-16x16.png"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#131414"
       />
     </NextHead>
   )
